d251c97aec3be033e34c42520c46d2bdfa4ede58
skatedog <skatedog0001@gmail.com>
2021-12-17 14:56:03 +0900

    [Update]リファクタリング,並び順変更

--- taskleaf/app/controllers/tasks_controller.rb
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.recent
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -21,11 +21,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    @task = current_user.tasks.find(params[:id])
     if @task.update(task_params)
       redirect_to @task, notice: "タスク「#{@task.name}」を更新しました。"
     else
@@ -34,12 +32,14 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
-    task.destroy
-    redirect_to tasks_path, notice: "タスク「#{task.name}」を削除しました。"
+    @task.destroy
+    redirect_to tasks_path, notice: "タスク「#{@task.name}」を削除しました。"
   end
-
+  
   private
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
   def task_params
     params.require(:task).permit(:name, :description)
   end
