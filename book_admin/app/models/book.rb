5472a783d65d541715a28ea38cc5f8861810fd35
skatedog <skatedog0001@gmail.com>
2021-12-22 15:51:44 +0900

    [Add]validation

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
   belongs_to :user
 
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :price, numericality: { greater_then_or_equal_to: 0 }
+
   scope :costly, -> { where('price > ?', 3000) }
   scope :written_about, ->(theme) { where('name like ?', "%#{theme}%") }
 end
